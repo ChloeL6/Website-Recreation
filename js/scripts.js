d1865469354ee8e1c2dcb89723a718d77307a7e8
Chloe Le <yenchi.le1199@gmail.com>
2022-10-24 14:00:37 -0700

    Add ul,h2,a on js

--- js/scripts.js
+++ js/scripts.js
@@ -19,11 +19,29 @@ window.addEventListener("load", function () {
   p.after(img);
 
   const newH1 = document.createElement("h1");
-  h1.append("Facts about Multicolored Tanager");
-  img.after(h1);
+  newH1.append("Facts about Multicolored Tanager");
+  img.after(newH1);
 
+
+  const li1 = document.createElement("li");
+  const li2 = document.createElement("li");
+  li1.append("It is endemic to the mountains of Columbia.");
+  li2.append("It usually searches for insects on the underside of leaves of outer limbs while clinging to leaves with its feet.")
+  const ul = document.createElement("ul");
+  ul.prepend(li1);
+  ul.append(li2);
+  newH1.after(ul);
+
+  const h2 = document.createElement("h2");
+  h2.append("Source");
+  ul.after(h2);
+
+  const a = document.createElement("a");
+  a.setAttribute("href", "https://en.wikipedia.org/wiki/Multicoloured_tanager");
   
-
+  const textNode = document.createTextNode("Wikipedia");
+  a.appendChild(textNode);
+  h2.after(a);
 
 });
 
